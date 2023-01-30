cc261f40285ea31818da4ed9fbed2504308f2a9e
skt231 <shimguta123@gmail.com>
2023-01-31 04:46:41 +0900

    udspring

--- spring/udemySpring/00/01basic/spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring/udemySpring/00/01basic/spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,14 +1,49 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	//define a default construcotr
+	public TennisCoach() {
+		System.out.println(">> TennisCoach : inside default constructor");
+	}
+	
+	@Autowired
+	public void doSomeCrazyStuff(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach : inside default constructor");
+		fortuneService=theFortuneService;
+	}
+	/*
+	 * //define a setter method
+	 * 
+	 * @Autowired public void setFortuneService(FortuneService theFortuneService) {
+	 * fortuneService= theFortuneService; }
+	 */
+	
+	
+	//Spring will scan for component that implements FortuneService interface
+	//	our example: HappyFortuneService meets the requirement
+	/*
+	 * @Autowired public TennisCoach(FortuneService theFortuneService) {
+	 * fortuneService = theFortuneService; }
+	 */
+	
 	@Override
 	public String getDailyWorkout() {
 		// TODO Auto-generated method stub
 		return "Practice your backhand volley";
 	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune();
+	}
+	
 
 }
