5dc8e1a72d75d589bd25d476f3699c9947c14b03
skt231 <shimguta123@gmail.com>
2023-03-14 16:12:54 +0900

    0314_1

--- java/programers/zerostage/src/zerostage/del_vowel.java
+++ java/programers/zerostage/src/zerostage/del_vowel.java
@@ -1,25 +1,28 @@
 package zerostage;
 
+//영어에선 a, e, i, o, u 다섯 가지 알파벳을 모음으로 분류합니다.
+//문자열 my_string이 매개변수로 주어질 때 모음을 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
 public class del_vowel {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		solution("abcde");
 
 	}
-	 public static void solution(String my_string) {
-         String answer = "";
-         String[] stringArr={};
-         char[] vowel={'a','e','i','o','u'};
-     for(int i=0;i<my_string.length();i++){
-         stringArr[i]=my_string.charAt(i);
-     }//ㅁㅜㄴㅈㅏㅇㅕㄹㅇㅡㄹ ㅂㅐㅇㅕㄹㄹㅗ
-         for(int i=0;i<stringArr.length;i++){
-             for(int j=0;j<vowel.length;j++){
-                 if(stringArr[i].equals(vowel[j])){
-                 answer=stringArr.replace(stringArr[i],"");   
-             }
+
+	public static void solution(String my_string) {
+		String answer = "";
+		String[] vowel = { "a", "e", "i", "o", "u" };
+		char[] stringArr = my_string.toCharArray();
+		for (int i = 0; i < stringArr.length; i++) {
+			if( my_string.contains(vowel[i])) {
+				answer = my_string.replaceAll(vowel[i], "");
+				my_string = answer;
+			} else {
+                answer =my_string;
             }
-         }
-     System.out.println(answer); 
- }
+		}
+        
+        
+        System.out.println(answer);
+    }
 }
--- java/programers/zerostage/src/zerostage/repeat_char.java
+++ java/programers/zerostage/src/zerostage/repeat_char.java
@@ -3,8 +3,19 @@ package zerostage;
 public class repeat_char {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String my_string="ABCDE";
+		solution(my_string,2);
 
 	}
+	 public static void solution(String my_string,int n) {
+	        String answer = "";
+	        char[] arr = my_string.toCharArray();
+	        for(int i=0;i<arr.length;i++) {
+	        	for(int j=0;j<n;j++) {
+	        		answer += arr[i];
+	        	}
+	        }
+	        System.out.println(answer);
+	    }
 
 }
