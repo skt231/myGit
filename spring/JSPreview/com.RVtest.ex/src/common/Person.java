eed4b7ccf1ad59e6dfc73ee8af83abb8a5f44f41
skt231 <shimguta123@gmail.com>
2023-03-21 17:16:36 +0900

    03-21_1

---
+++ spring/JSPreview/com.RVtest.ex/src/common/Person.java
@@ -0,0 +1,26 @@
+package common;
+
+public class Person {
+	private String name;
+	public String getName() {
+		return name;
+	}
+	
+	public Person(){}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	private int age;
+}
