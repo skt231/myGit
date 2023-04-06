62e9cce9068ff0b645b96dcee74f4fe95a72b16b
skt231 <shimguta123@gmail.com>
2023-04-06 17:11:13 +0900

    04-06_1

--- java/programers/zerostage/src/test/sum_Array.java
+++ java/programers/zerostage/src/test/sum_Array.java
@@ -12,20 +12,20 @@ public class sum_Array {
 
 	public static void quest() {
 		Scanner scan = new Scanner(System.in);
-		int N = scan.nextInt();
+		int N = scan.nextInt();// 자연수 입력
 		int count = 1;
 		int start_index = 1;
 		int end_index = 1;
 		int sum = 1;
 		while (end_index != N) {
-			if (sum == N) {
+			if (sum == N) { // 현재 연속 합이 N과 같은 경우
 				count++;
 				end_index++;
 				sum += end_index;
-			} else if (sum > N) {
+			} else if (sum > N) { // 현재 연속 합이 N보다 큰 경우
 				sum -= start_index;
 				start_index++;
-			} else {
+			} else { // 현재 연속 합이 N보다 작은 경우
 				end_index++;
 				sum += end_index;
 			}
