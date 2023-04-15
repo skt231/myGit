b9c10a4bb15a6e634fc16adea4f59660642e1abf
skt231 <shimguta123@gmail.com>
2023-04-15 21:30:04 +0900

    04-15

--- java/programers/zerostage/src/test/maxNum_2.java
+++ java/programers/zerostage/src/test/maxNum_2.java
@@ -1,13 +1,15 @@
 package test;
 
 public class maxNum_2 {
-	//정수 배열 numbers가 매개변수로 주어진다.numbers의 원소중 두개를 곱해 만들수 있는 최댃값을 return하도록 solution 함수를 완성하라.
+	// 정수 배열 numbers가 매개변수로 주어진다.numbers의 원소중 두개를 곱해 만들수 있는 최댃값을 return하도록 solution
+	// 함수를 완성하라.
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] arr = { 1, 3, 5, 6, 7 };
+		System.out.println(solution(arr));
 
 	}
 
-	public int solution(int[] numbers) {
+	public static int solution(int[] numbers) {
 		int answer = 0;
 		int complex = 0;
 		int complex_max = 0;
